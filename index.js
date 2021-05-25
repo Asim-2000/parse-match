1282892090d0e8f9978631238c5c4be6e57b52ee
Muhammad Asim Khaskheli <mkhaskheli.bese18seecs@seecs.edu.pk>
2021-05-26 02:47:33 +0500

    👌 IMPROVE: parseScript

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var esprima = require('esprima')
+var esprima = require('esprima');
 const fs = require("fs");
 const source = fs.readFileSync("./parsed.js", "utf8");
 
-tokenized = esprima.tokenize(source);
+tokenized = esprima.(source);
 console.log()
 
-fs.writeFile("tokenized.json", JSON.stringify(tokenized), (err) => {
+fs.writeFile("parsedTest.js", JSON.stringify(tokenized), (err) => {
   // throws an error, you could also catch it here
   if (err) throw err;
 
